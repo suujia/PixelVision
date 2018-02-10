f90845cfe47b730b8d52e7d699b304ef124c7314
mitchellang <ang_mitchell@yahoo.com>
2018-02-10 01:51:00 -0800

    Used a better version of spectrogram code

--- audio_processing.py
+++ audio_processing.py
@@ -1,88 +1,29 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import scipy.io.wavfile as wav
-from numpy.lib import stride_tricks
+import matplotlib.pyplot as plot
 
-#TODO: Review code and modify
+from scipy.io import wavfile
 
+# Read the wav file (mono)
 
-def stft(sig, frameSize, overlapFac = 0.5, window = np.hanning):
-    win = window(frameSize)
-    hopSize = int(frameSize - np.floor(overlapFac * frameSize))
+samplingFrequency, signalData = wavfile.read('test3.wav')
 
-    # zeros at beginning (thus center of 1st window should be for sample nr. 0)
-    samples = np.append(np.zeros(np.floor(frameSize / 2.0)), sig)
-    # cols for windowing
-    cols = np.ceil((len(samples) - frameSize) / float(hopSize)) + 1
-    # zeros at end (thus samples can be fully covered by frames)
-    samples = np.append(samples, np.zeros(frameSize))
+# Plot the signal read from wav file
 
-    frames = stride_tricks.as_strided(samples, shape = (cols, frameSize),
-                                      strides = (samples.strides[0] * hopSize, samples.strides[0])).copy()
-    frames *= win
+plot.subplot(211)
 
-    return np.fft.rfft(frames)
+plot.title('Spectrogram of a wav file with piano music')
 
+plot.plot(signalData)
 
-""" scale frequency axis logarithmically """
+plot.xlabel('Sample')
 
+plot.ylabel('Amplitude')
 
-def logscale_spec(spec, sr = 44100, factor = 20.):
-    timebins, freqbins = np.shape(spec)
+plot.subplot(212)
 
-    scale = np.linspace(0, 1, freqbins) ** factor
-    scale *= (freqbins - 1) / max(scale)
-    scale = np.unique(np.round(scale))
+plot.specgram(signalData, Fs = samplingFrequency, mode = 'default')
 
-    # create spectrogram with new freq bins
-    newspec = np.complex128(np.zeros([timebins, len(scale)]))
-    for i in range(0, len(scale)):
-        if i == len(scale) - 1:
-            newspec[:, i] = np.sum(spec[:, scale[i]:], axis = 1)
-        else:
-            newspec[:, i] = np.sum(spec[:, scale[i]:scale[i + 1]], axis = 1)
+plot.xlabel('Time')
 
-    # list center freq of bins
-    allfreqs = np.abs(np.fft.fftfreq(freqbins * 2, 1. / sr)[:freqbins + 1])
-    freqs = []
-    for i in range(0, len(scale)):
-        if i == len(scale) - 1:
-            freqs += [np.mean(allfreqs[scale[i]:])]
-        else:
-            freqs += [np.mean(allfreqs[scale[i]:scale[i + 1]])]
+plot.ylabel('Frequency')
 
-    return newspec, freqs
-
-
-""" plot spectrogram"""
-
-
-def plotstft(audiopath, binsize = 2 ** 10, plotpath = None, colormap = "jet"):
-    samplerate, samples = wav.read(audiopath)
-    s = stft(samples, binsize)
-
-    sshow, freq = logscale_spec(s, factor = 1.0, sr = samplerate)
-    ims = 20. * np.log10(np.abs(sshow) / 10e-6)  # amplitude to decibel
-
-    timebins, freqbins = np.shape(ims)
-
-    plt.figure(figsize = (15, 7.5))
-    plt.imshow(np.transpose(ims), origin = "lower", aspect = "auto", cmap = colormap, interpolation = "none")
-    plt.colorbar()
-
-    plt.xlabel("time (s)")
-    plt.ylabel("frequency (hz)")
-    plt.xlim([0, timebins - 1])
-    plt.ylim([0, freqbins])
-
-    xlocs = np.float32(np.linspace(0, timebins - 1, 5))
-    plt.xticks(xlocs, ["%.02f" % l for l in ((xlocs * len(samples) / timebins) + (0.5 * binsize)) / samplerate])
-    ylocs = np.int16(np.round(np.linspace(0, freqbins - 1, 10)))
-    plt.yticks(ylocs, ["%.02f" % freq[i] for i in ylocs])
-
-    if plotpath:
-        plt.savefig(plotpath, bbox_inches = "tight")
-    else:
-        plt.show()
-
-    plt.clf()
+plot.show()
