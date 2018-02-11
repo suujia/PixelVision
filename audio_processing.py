ae75e905a0167082d271aa83fa2af9ce2c336762
mitchellang <ang_mitchell@yahoo.com>
2018-02-11 15:07:42 -0800

    added research paper

--- audio_processing.py
+++ audio_processing.py
@@ -6,21 +6,33 @@ from scipy.io import wavfile
 
 samplingFrequency, signalData = wavfile.read('test3.wav')
 
+samplingFrequency1, signalData1 = wavfile.read('test2.wav')
+
+samplingFrequency2, signalData2 = wavfile.read('test1.wav')
+
 # Plot the signal read from wav file
 
-plot.subplot(211)
+plot.subplot(311)
 
 plot.title('Spectrogram of a wav file with piano music')
 
-plot.plot(signalData)
+plot.specgram(signalData1, Fs = samplingFrequency1, mode = 'default')
 
 plot.xlabel('Sample')
 
 plot.ylabel('Amplitude')
 
-plot.subplot(212)
+plot.subplot(312)
 
 plot.specgram(signalData, Fs = samplingFrequency, mode = 'default')
+
+plot.xlabel('Time')
+
+plot.ylabel('Frequency')
+
+plot.subplot(313)
+
+plot.specgram(signalData2, Fs = samplingFrequency2, mode = 'default')
 
 plot.xlabel('Time')
 
